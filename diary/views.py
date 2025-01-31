32275455244e9af3cbf6a9cbc98b06855893532d
kengoMBP <kengo0805ooo@gmail.com>
2025-01-31 23:34:20 +0900

    add:日記作成機能の追加
    Co-authored-by: Kiyonobu Yuya <yuuyayukinanana@users.noreply.github.com>

--- diary/views.py
+++ diary/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import InquiryForm
 from .models import Diary
+from .forms import InquiryForm, DiaryCreateForm
 
 logger = logging.getLogger(__name__)
 
@@ -39,3 +40,26 @@ class DiaryDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = 'diary_detail.html'
     # slug_field = "title" # モデルのフィールドの名前
     # slug_url_kwarg = "title" # urls.pyのキーワードの名前
+
+class DiaryCreateView(LoginRequiredMixin, generic.CreateView):
+    # モデルとテンプレートを設定
+    # モデルを設定
+    model = Diary
+    # テンプレートを設定
+    template_name = 'diary_create.html'
+    # フォームを設定
+    form_class = DiaryCreateForm
+    # 成功時のリダイレクト先を設定
+    success_url = reverse_lazy('diary:diary_list')
+
+    # ビジネスロジックを実装
+    def form_valid(self, form):
+        diary = form.save(commit=False)
+        diary.user = self.request.user
+        diary.save()
+        messages.success(self.request, '日記を作成しました。')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, '日記の作成に失敗しました。')
+        return super().form_invalid(form)
